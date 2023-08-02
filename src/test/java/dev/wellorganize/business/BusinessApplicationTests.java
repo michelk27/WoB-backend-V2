7985ab198acf5b3e34b8d536538f822457d7b00a
MKcode27 <kanga.michel89@gmail.com>
2023-08-02 12:07:11 -0400

    wob backend setup

---
+++ src/test/java/dev/wellorganize/business/BusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wellorganize.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
